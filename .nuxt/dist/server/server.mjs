a7848163548dc5241b850784ae1b0cf5b49edd54
Helena <lusichelena@gmail.com>
2024-07-24 13:59:58 +0200

    test version

---
+++ .nuxt/dist/server/server.mjs
@@ -0,0 +1,1 @@
+export { default } from "file:///C:/Users/Test/Desktop/Posao/flora/flora/node_modules/@nuxt/vite-builder/dist/runtime/vite-node.mjs"
---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+<header>
+        <h2>Dobrodošli u <i>Floru</i></h2>
+</header>
+<div>
+    <p>Vjerujemo u snagu prirode i njezina iscjeliteljska svojstva. </p>
+        <p>Samo najkvalitetniji biljni čajevi i tinkture.</p>
+</div>
+<footer>
+    Kontakt: +385 111 2233
+    florabotanica@gmail.com
+</footer>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+h2 {
+    margin-bottom: 20px;
+    font-size: 36px;
+}
+p{
+    margin: 20px 0;
+}
+footer{
+    font-size: 15px;
+}
+</style>
